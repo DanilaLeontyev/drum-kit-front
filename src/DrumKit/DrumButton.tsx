9882faa677d62a49aab26e4f42ccadc9e6f7c616
Danila Leontyev <danila.leontyev.web@gmail.com>
2020-04-08 22:38:19 +0500

    refactoring

--- src/DrumKit/DrumButton.tsx
+++ src/DrumKit/DrumButton.tsx
@@ -5,9 +5,17 @@ type DrumButtonProps = {
   button: DrumButtonInterface;
 }
 
+const initState = {
+  buttonClassName: ['SoundButton'],
+}
+
 export const DrumButton = (props: DrumButtonProps) => {
   const audioRef: React.RefObject<HTMLAudioElement> = React.useRef(null);
   const buttonRef: React.RefObject<HTMLButtonElement> = React.useRef(null);
+
+
+  const [buttonClassName, setButtonClassName] =
+    React.useState<string[]>(initState.buttonClassName)
 
   async function callAfterDelay<T>(sleppTime: number, fn: (args: T) => any, args: T) {
     return await setTimeout(async () => {
@@ -16,30 +24,27 @@ export const DrumButton = (props: DrumButtonProps) => {
   }
 
   const reverseStyle = (audio: HTMLAudioElement) => {
-    buttonRef?.current?.classList.remove("SoundButton__playable")
+    setButtonClassName(initState.buttonClassName)
     if (audio.parentElement) {
       audio.parentElement.innerHTML = props.button.name;
     }
   }
 
   const changeStyle = (audio: HTMLAudioElement) => {
-    buttonRef?.current?.classList.add("SoundButton__playable")
+    setButtonClassName([...buttonClassName, 'SoundButton__playable'])
     callAfterDelay<HTMLAudioElement>(audio.duration * 1000, reverseStyle, audio)
   }
 
   const onPlayClick = () => {
-    const audio = audioRef?.current
+    const audio = audioRef?.current;
     if (audio) {
-      if (audio.played) {
-        audio.currentTime = 0;
-      }
       changeStyle(audio);
       audio.play();
     };
   }
 
   return (
-    <button ref={buttonRef} className="SoundButton" onClick={onPlayClick}>
+    <button ref={buttonRef} className={buttonClassName.join(' ')} onClick={onPlayClick}>
       {props.button.name}
       <audio ref={audioRef} src={process.env.PUBLIC_URL + props.button.sound} />
     </button>
