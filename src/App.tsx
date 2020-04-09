eb7eb89c204437bdb82e51707e813bfa28cb29da
Danila Leontyev <danila.leontyev.web@gmail.com>
2020-04-09 23:54:43 +0500

    add css reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import { DrumKit } from './DrumKit';
-import './index.css'
+import './reset.css';
+import './index.css';
 
 function App() {
   return (
